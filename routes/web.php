cf69318c88cb36d5053153c6f5154bac19549c93
Huyhq <huyhq@deha-soft.com>
2022-05-06 11:10:52 +0700

    add layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard.index');
+});
+Route::get('/home', function () {
+    return view('client.layouts.app');
+});
+Auth::routes();
